e241a0e32e082d0f27170dd24fdc67935af3836e
GabrielCC163 <gbrum.rodrigues@gmail.com>
2020-11-26 21:04:52 -0300

    feat: users, addresses, techs

---
+++ STEPS.md
@@ -0,0 +1,12 @@
+sudo mkdir -p /storage/docker/sqlnode/postgresql-data
+
+docker run --name docker-sqlnode -e POSTGRES_PASSWORD=docker -p 5432:5432 -v /storage/docker/sqlnode/postgresql-data:/var/lib/postgresql/data -d postgres
+
+Create a .sequelizerc file and set up configuration (src/config/database.js)
+
+Create database: yarn sequelize db:create
+
+Create migration: yarn sequelize migration:create --name=create-users
+
+Run migrations: yarn sequelize db:migrate
+rollback locally last migration run: yarn sequelize db:migrate:undo
---
+++ src/database/migrations/20201126225247-create-addresses.js
@@ -0,0 +1,48 @@
+"use strict";
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable("addresses", {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+      },
+      user_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: "users",
+          key: "id",
+        },
+        onUpdate: "CASCADE", //when there is some change on the User id
+        onDelete: "CASCADE", //delete address when User is deleted
+      },
+      zipcode: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      street: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      number: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable("addresses");
+  },
+};
